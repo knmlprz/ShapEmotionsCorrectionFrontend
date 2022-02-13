18fbb7bc235eca8f2e9b33af34f9113b27f52df0
Restyled.io <commits@restyled.io>
2022-02-13 12:22:35 +0000

    Restyled by clang-format

--- src/scripts/explain-sentiment.js
+++ src/scripts/explain-sentiment.js
@@ -1,63 +1,67 @@
 "use strict";
 import colorGradient from "javascript-color-gradient";
-import { post } from "./utils.js"
+import {post} from "./utils.js"
 
 // Create gradient array
-const gradientArray = colorGradient.setGradient("#4F94FF","#ffffff", "FF512C").setMidpoint(11).getArray();
+const gradientArray = colorGradient.setGradient("#4F94FF", "#ffffff", "FF512C")
+                          .setMidpoint(11)
+                          .getArray();
 console.log(gradientArray);
 
 // Get input element
 let input = document.querySelector('#explain');
 
 async function explainSentiment() {
-    let text = document.getElementById("data").value;
-    console.log(text)
-    post('http://localhost:8000/sentiment', data={"value": text})
-        .then(data => {
-            console.log("Sentiment: " + data.label);
-            writePredictions(text, data.label)
-        });
+  let text = document.getElementById("data").value;
+  console.log(text)
+  post('http://localhost:8000/sentiment', data = {
+    "value" : text
+  }).then(data => {
+    console.log("Sentiment: " + data.label);
+    writePredictions(text, data.label)
+  });
 }
 
 function map_to_10s(number = 0.0) {
-    const lim = 10;
-    const mult = 100;
-    let res = number * mult + lim / 2;
-    if (res > lim) return lim;
-    if (res < 0) return 0;
-    return Math.round(res);
+  const lim = 10;
+  const mult = 100;
+  let res = number * mult + lim / 2;
+  if (res > lim)
+    return lim;
+  if (res < 0)
+    return 0;
+  return Math.round(res);
 }
-
 
 async function writePredictions(text = '', label = '') {
-    // Get data from server
-    const response = await post('http://localhost:8000/sentiment/explain', data = {"value": text});
-    console.log(response)
-    
-    // Clear any previous predictions
-    let container = document.querySelector('#explanation-container')
-    container.innerHTML = '';
+  // Get data from server
+  const response = await post('http://localhost:8000/sentiment/explain',
+                              data = {"value" : text});
+  console.log(response)
 
-    // Map values to 0 - 10 (int)
-    let shapley_values = response[label].map(map_to_10s)
-    console.log(shapley_values);
+  // Clear any previous predictions
+  let container = document.querySelector('#explanation-container')
+  container.innerHTML = '';
 
-    // Create span for every prediction
-    response.text.forEach(function(item, index, array) {
-        let spanobj = document.createElement("span");
-        spanobj.innerText = item;
-        spanobj.style.backgroundColor = gradientArray[shapley_values[index]];
+  // Map values to 0 - 10 (int)
+  let shapley_values = response[label].map(map_to_10s)
+  console.log(shapley_values);
 
+  // Create span for every prediction
+  response.text.forEach(function(item, index, array) {
+    let spanobj = document.createElement("span");
+    spanobj.innerText = item;
+    spanobj.style.backgroundColor = gradientArray[shapley_values[index]];
 
-        container.appendChild(spanobj);
-    });
+    container.appendChild(spanobj);
+  });
 
-    // Let user click element
-    input.removeAttribute('disabled')
+  // Let user click element
+  input.removeAttribute('disabled')
 }
 
-input.addEventListener('click', function (e) {
-    input.setAttribute('disabled', 'true');
-    // Explain sentiment
-    explainSentiment();
-  });
+input.addEventListener('click', function(e) {
+  input.setAttribute('disabled', 'true');
+  // Explain sentiment
+  explainSentiment();
+});
--- src/scripts/predict-sentiment.js
+++ src/scripts/predict-sentiment.js
@@ -1,24 +1,25 @@
 "use strict";
-import { post } from "./utils.js"
+import {post} from "./utils.js"
 
 function readSentiment() {
-    let text = document.getElementById("data").value;
-    console.log("Predicting sentiment");
-    post('http://localhost:8000/sentiment', data={"value": text})
-        .then(data => {
-            console.log(data);
-            changeTextareaColor(data.label, data.score);
-        });
+  let text = document.getElementById("data").value;
+  console.log("Predicting sentiment");
+  post('http://localhost:8000/sentiment', data = {
+    "value" : text
+  }).then(data => {
+    console.log(data);
+    changeTextareaColor(data.label, data.score);
+  });
 };
 
 function changeTextareaColor(label = '', score = 0.0) {
-    if(label == "neutral" ) {
-      document.getElementById("data").style.color = "black";
-    } else if (label == "negative" ) {
-      document.getElementById("data").style.color = "red";
-    } else {
-      document.getElementById("data").style.color = "green";
-    }
+  if (label == "neutral") {
+    document.getElementById("data").style.color = "black";
+  } else if (label == "negative") {
+    document.getElementById("data").style.color = "red";
+  } else {
+    document.getElementById("data").style.color = "green";
+  }
 }
 
 let input = document.querySelector('textarea');
@@ -27,14 +28,12 @@ let input = document.querySelector('textarea');
 let timeout = null;
 
 // Listen for keystroke events
-input.addEventListener('keyup', function (e) {
+input.addEventListener('keyup', function(e) {
   // Clear the timeout if it has already been set.
   // This will prevent the previous task from executing
   // if it has been less than <MILLISECONDS>
   clearTimeout(timeout);
 
   // Make a new timeout set to go off in 1000ms (1 second)
-  timeout = setTimeout(function () {
-    readSentiment()
-  }, 300);
+  timeout = setTimeout(function() { readSentiment() }, 300);
 });
