a0c989e5925724d45de4c0222fd197b02f61b2e5
Piotr Krawiec <piotr.krawiec23@gmail.com>
2022-02-13 10:56:54 +0100

    Refactor js

--- src/scripts/textarea-colors.js
+++ src/scripts/textarea-colors.js
@@ -1,18 +1,19 @@
 "use strict";
 import Gradient from "javascript-color-gradient";
+import { post } from "./utils.js"
 
-function readData() {
+function readSentiment() {
     let text = document.getElementById("data").value;
     console.log(data);
     console.log("heys");
-    postData('http://localhost:8000/sentiment', data={"value": text})
+    post('http://localhost:8000/sentiment', data={"value": text})
         .then(data => {
             console.log(data);
-            changeColor(data.label, data.score);
+            changeTextareaColor(data.label, data.score);
         });
 };
 
-function changeColor(label = '', score = 0.0) {
+function changeTextareaColor(label = '', score = 0.0) {
     console.log(label);
 
     if(label == "neutral" ) {
@@ -24,19 +25,4 @@ function changeColor(label = '', score = 0.0) {
     }
 }
 
-async function postData(url = '', data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-
-document.querySelector('textarea').addEventListener('input', readData);
+document.querySelector('textarea').addEventListener('input', readSentiment);
